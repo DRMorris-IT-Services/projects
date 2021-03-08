a7a17290d8805892831757e53bd5a787f268b63c
Duncan Morris <d.morris@drmorris-itservices.de>
2021-03-08 13:34:26 +0000

    repo setup

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,17 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+ #### PROJECTS MODULE ####
+ Route::get('projects', 'ProjectsController@index')->name('projects');
+ Route::get('projects/new', 'ProjectsController@create')->name('projects.new');
+ Route::put('projects/add', 'ProjectsController@store')->name('projects.add');
+ Route::get('projects/view/{id}', 'ProjectsController@show')->name('projects.view');
+ Route::get('projects/edit/{id}', 'ProjectsController@edit')->name('projects.edit');
+ Route::put('projects/update/{id}', 'ProjectsController@update')->name('projects.update');
+ Route::put('projects/del/{id}', 'ProjectsController@destroy')->name('projects.del');
+
+ #### PROJECTS TASKS ####
+ Route::put('projects/tasks/add/{id}/{cid}', 'ProjectTaskController@store')->name('projects.addtask');
+ Route::put('projects/tasks/update/{id}/{cid}', 'ProjectTaskController@update')->name('projects.updatetask');
+ Route::put('projects/tasks/del/{id}/{cid}', 'ProjectTaskController@destroy')->name('projects.deltask');
