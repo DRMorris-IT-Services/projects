59fb3276f18f447a7cc87773dc1d8496932f2111
Duncan Morris <duncanmorris07@outlook.com>
2021-03-15 17:41:05 +0100

    secure routes

--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/releasenotes', 'HomeController@releasenotes')->name('releasenotes');
-Route::get('/help', 'HomeController@help')->name('help');
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
 
  #### PROJECTS MODULE ####
- Route::get('projects', 'ProjectsController@index')->name('projects');
- Route::get('projects/new', 'ProjectsController@create')->name('projects.new');
- Route::put('projects/add', 'ProjectsController@store')->name('projects.add');
- Route::get('projects/view/{id}', 'ProjectsController@show')->name('projects.view');
- Route::get('projects/edit/{id}', 'ProjectsController@edit')->name('projects.edit');
- Route::put('projects/update/{id}', 'ProjectsController@update')->name('projects.update');
- Route::put('projects/del/{id}', 'ProjectsController@destroy')->name('projects.del');
+ Route::get('projects', 'ProjectsController@index')->name('projects')->middleware('auth');
+ Route::get('projects/new', 'ProjectsController@create')->name('projects.new')->middleware('auth');
+ Route::put('projects/add', 'ProjectsController@store')->name('projects.add')->middleware('auth');
+ Route::get('projects/view/{id}', 'ProjectsController@show')->name('projects.view')->middleware('auth');
+ Route::get('projects/edit/{id}', 'ProjectsController@edit')->name('projects.edit')->middleware('auth');
+ Route::put('projects/update/{id}', 'ProjectsController@update')->name('projects.update')->middleware('auth');
+ Route::put('projects/del/{id}', 'ProjectsController@destroy')->name('projects.del')->middleware('auth');
 
  #### PROJECTS TASKS ####
- Route::put('projects/tasks/add/{id}/{cid}', 'ProjectTaskController@store')->name('projects.addtask');
- Route::put('projects/tasks/update/{id}/{cid}', 'ProjectTaskController@update')->name('projects.updatetask');
- Route::put('projects/tasks/del/{id}/{cid}', 'ProjectTaskController@destroy')->name('projects.deltask');
+ Route::put('projects/tasks/add/{id}/{cid}', 'ProjectTaskController@store')->name('projects.addtask')->middleware('auth');
+ Route::put('projects/tasks/update/{id}/{cid}', 'ProjectTaskController@update')->name('projects.updatetask')->middleware('auth');
+ Route::put('projects/tasks/del/{id}/{cid}', 'ProjectTaskController@destroy')->name('projects.deltask')->middleware('auth');
